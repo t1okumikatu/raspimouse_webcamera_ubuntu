2b73a6ca987ddb8d3daaffacf004292924b6ca65
yoshi <t1okumikatu@gmail.com>
2019-11-28 17:13:19 +0900

    pimouse_rosとraspimouse_game_controller
    raspimoueにwebcameraを装着
    ubuntu1804

---
+++ webmouse/pimouse_ros/scripts/buzzer1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import rospy
+rospy.init_node('buzzer')
+rospy.spin()
+
+# Copyright 2016 Ryuichi Ueda
+# Released under the BSD License.
+# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
